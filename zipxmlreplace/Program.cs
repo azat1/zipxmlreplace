b5fdf8cc6482f3efc2de2418b45b5ee389fc5d6e
unknown <azamatov@bk.ru>
2024-04-14 11:51:29 +0500

    first

---
+++ zipxmlreplace/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using zipxmlreplace;
+
+string templ = args[0];
+string from = args[1];
+string to= args[2];
+ZipReplacer.Replace(Directory.GetCurrentDirectory(),templ,from, to);
+Console.WriteLine(args[0]);
---
+++ zipxmlreplace/ZipReplacer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zipxmlreplace
+{
+    public static class ZipReplacer
+    {
+        public static void Replace(string dir,string template,string from, string to)
+        {
+            var files=Directory.EnumerateFiles(dir, template);
+            foreach (var file in files) {
+                ReplaceOneZIP(file,from,to);
+            }
+            var dirs = Directory.EnumerateDirectories(dir);
+            foreach (var vdir in dirs) {  
+                Replace(vdir,template,from,to);
+            }
+
+        }
+
+        private static void ReplaceOneZIP(string file, string from, string to)
+        {
+            Console.WriteLine(file);
+            //return;
+            FileStream stream = new FileStream(file, FileMode.Open);
+            ZipArchive zip=new ZipArchive(stream,ZipArchiveMode.Update);
+            foreach (ZipArchiveEntry entry in zip.Entries)
+            {
+               if (entry.Name.ToLower().EndsWith(".xml"))
+                {
+                    string entryname =entry.Name;
+                    Console.WriteLine(entryname);
+                    Stream xxx = entry.Open();
+                    StreamReader reader=new StreamReader(xxx,Encoding.UTF8);
+                    string line=reader.ReadToEnd();
+                    reader.Close();
+                    entry.Delete();
+                    string newline = line.Replace(from, to);
+                    ZipArchiveEntry newentry= zip.CreateEntry(entryname);
+                    xxx=newentry.Open();
+                    StreamWriter wrt = new StreamWriter(xxx,Encoding.UTF8);
+                    wrt.Write(newline); 
+                    wrt.Close();
+                    break;
+                }
+            }
+            zip.Dispose();
+            stream.Close();
+        }
+    }
+}
